af7ac1bd1a233978f7238f20586b9047a9ab2738
Frederic Sauve Hoover <rsauveho@ualberta.ca>
2019-03-03 12:59:08 -0700

    don't think I need django heroku package for travis deployment

--- Cmput404Project/settings.py
+++ Cmput404Project/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
-import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -119,5 +118,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-django_heroku.settings(locals())
