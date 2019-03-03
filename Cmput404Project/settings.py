0bb5b5b07ce79ab2aa868c324aad9d5dc698735d
Frederic Sauve Hoover <rsauveho@ualberta.ca>
2019-03-03 13:12:08 -0700

    probably needed it after all

--- Cmput404Project/settings.py
+++ Cmput404Project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -118,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
