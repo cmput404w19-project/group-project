6525ac525b1223f9f95e1594212f7d16ea72a865
ExploreNcrack <zhaozhen@ualberta.ca>
2019-03-03 18:50:58 -0700

    organize accounts templates folder

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,8 @@
 # accounts/urls.py
 from django.urls import path
+from . import views # from current directory to look for views file
 
-from . import views
-
-
+# has to be called urlpatterns 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
 ]
