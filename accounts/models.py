f72f360679278c3190b1e7d22434ad1eeb639614
ExploreNcrack <zhaozhen@ualberta.ca>
2019-03-07 17:00:01 -0700

    .

--- accounts/models.py
+++ accounts/models.py
@@ -5,12 +5,30 @@ from django.db.models.signals import post_save
 # Create your models here.
 class UserProfile(models.Model):
 	user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
+	username = models.CharField(max_length=20, default="") 
 	description = models.CharField(max_length=100, default="")
-	city = models.CharField(max_length=100, default="")
-	host = models.CharField(max_length=100, default="")
+	city = models.CharField(max_length=30, default="")
+	host = models.CharField(max_length=50, default="")
 	website = models.URLField(default="")
 	phone = models.IntegerField(default=0)
+	class Meta:
+		indexes = [
+			models.Index(fields=['user'], name='usr_idx'),
+		]
 
+class Post(models.Model):
+	# author   Foreign key to user
+	# title
+	# source
+	# origin
+	# description
+	pass
+
+class FriendList(models.Model):
+	pass
+
+class Images(models.Model):
+	pass
 
 def create_profile(sender, **kwargs):
 	if kwargs['created']:
