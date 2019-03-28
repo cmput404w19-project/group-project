76e52545c25ac4e27768c455c48b1333ba3dcdff
ExploreNcrack <zhaozhen@ualberta.ca>
2019-03-28 11:21:34 -0600

    fix comment hyperlink to and also fix now superuser also has userprofile but no host fill in

--- accounts/models.py
+++ accounts/models.py
@@ -193,11 +193,9 @@ class ExternalServer(models.Model):
 # using signal to tigger to create
 # https://simpleisbetterthancomplex.com/tutorial/2016/07/28/how-to-create-django-signals.html
 # author: Vitor Freitas
-# def create_profile(sender, **kwargs):
-#     if kwargs['created']:
-#         user_profile = UserProfile.objects.create(user_id=kwargs['instance'], displayName=kwargs['instance'].username)
-#         if not kwargs['instance'].is_staff:
-#             kwargs['instance'].is_active = False
-#             kwargs['instance'].save()
+def create_profile(sender, **kwargs):
+    # creat userprofile on superuser creation
+    if kwargs['created'] and kwargs['instance'].is_superuser:
+        user_profile = UserProfile.objects.create(user_id=kwargs['instance'], displayName=kwargs['instance'].username)
 
-# post_save.connect(create_profile, sender=User)
+post_save.connect(create_profile, sender=User)
