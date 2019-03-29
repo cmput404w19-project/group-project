31888397a92e76d97292ed52d0157cfb8195a946
ExploreNcrack <zhaozhen@ualberta.ca>
2019-03-29 10:37:54 -0600

    my mistake chengze dalao is right

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -112,27 +112,23 @@ class GETCommentSerializer(serializers.ModelSerializer):
         # just use this tmp not to change later
         # TODO handle user not on server
 
-        user_id = obj.user_id.rstrip("/").split("/")[-1]
-        comment_author = UserProfile.objects.filter(author_id=user_id).first()
-        return GETProfileSerializer(comment_author).data
-
         # Reference:
         # https://www.kancloud.cn/thinkphp/python-guide/39426
         #user = User.objects.filter(username=obj.user_id).first()
-        # with request.urlopen(obj.user_id) as f:
-        #     data = f.read().decode('utf-8')
-        #     print(obj.user_id)
-        #     print('Status:', f.status, f.reason)
-        #     print('Data:', data)
-        #     data = json.loads(data)
-        #     #print(data)
-        #     #print(data['id'])
-        #     #print(data['host'])
-        #     #print(data['displayName'])
-        #     #print(data['url'])
-        #     #print(data['github'])
-        #     #comment_author = UserProfile.objects.filter(user_id=user).first()
-        #     return data
+        with request.urlopen(obj.user_id) as f:
+            data = f.read().decode('utf-8')
+            print(obj.user_id)
+            print('Status:', f.status, f.reason)
+            print('Data:', data)
+            data = json.loads(data)
+            #print(data)
+            #print(data['id'])
+            #print(data['host'])
+            #print(data['displayName'])
+            #print(data['url'])
+            #print(data['github'])
+            #comment_author = UserProfile.objects.filter(user_id=user).first()
+            return data
 
 
 
